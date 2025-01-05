0acfc450ff1d48bacb14851c954ad263a4ab0c3c
yashksaini-coder <ys3853428@gmail.com>
2025-01-05 14:20:43 +0530

    fix: update text styling and layout in Home component for improved readability

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,12 @@ export default function Home() {
       <div className="relative z-10 flex flex-col justify-center items-center">
         <RiTwitterXLine size={50} color="white" />
         <div className="flex flex-col justify-center items-center mt-7 w-full max-w-4xl py-3">
-        <TextAnimate animation="blurInUp" by="character" className="text-white text-4xl font-extrabold text-center mb-4 leading-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl">
-          Tweet Craft
-        </TextAnimate>
-          <p className="text-white text-2xl ">Tweet Craft is an AI-powered application built with Next.js that generates tweets based on user descriptions.</p>
+          <TextAnimate animation="blurInUp" by="character" className="text-white text-4xl font-extrabold text-center mb-4 leading-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl">
+            Tweet Craft
+          </TextAnimate>
+            <p className="text-white text-center text-lg sm:text-base md:text-lg lg:text-xl xl:text-2xl mt-4">
+            Tweet Craft is an AI-powered application built with Next.js that generates tweets based on user descriptions.
+            </p>
           <InteractiveForm />
         </div>
       </div>
