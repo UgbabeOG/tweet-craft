a7f79e5bc6328995c604bc0f8f6e3a8409ae9d94
UgbabeOG <ugbabe.og@gmail.com>
2025-04-21 18:06:31 +0100

    feat: add social media icons to the homepage and improve layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-import { RiTwitterXLine } from "react-icons/ri";
+import {
+  RiTwitterXLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiTiktokLine,
+} from "react-icons/ri";
 import InteractiveForm from "./components/InteractiveForm";
 import Particles from "../components/ui/particles";
 import { TextAnimate } from "../components/ui/text-animate";
@@ -8,7 +13,12 @@ export default function Home() {
     <div className="relative flex flex-col justify-center items-center min-h-screen bg-[#1a1a1a] pt-6 px-2 w-full">
       <Particles className="absolute inset-0 w-full h-full z-0" />
       <div className="relative z-10 flex flex-col justify-center items-center">
-        <RiTwitterXLine size={50} color="white" />
+        <div className="flex flex-wrap gap-4 justify-center items-center mt-4 mb-2">
+          <RiTwitterXLine size={50} color="white" />{" "}
+          <RiFacebookLine size={50} color="white" />{" "}
+          <RiInstagramLine size={50} color="white" />{" "}
+          <RiTiktokLine size={50} color="white" />
+        </div>
         <div className="flex flex-col justify-center items-center mt-7 w-full max-w-4xl py-3">
           <TextAnimate
             animation="blurInUp"
@@ -18,8 +28,9 @@ export default function Home() {
             Caption Generator
           </TextAnimate>
           <p className="text-white text-center text-lg sm:text-base md:text-lg lg:text-xl xl:text-2xl mt-4">
-          Generate engaging tweets or captions for your photos effortlessly using AI. 
-          Simply describe your idea, and let the AI craft the perfect caption for you!
+            Generate engaging tweets or captions for your photos effortlessly
+            using AI. Simply describe your idea, and let the AI craft the
+            perfect caption for you!
           </p>
           <InteractiveForm />
         </div>
